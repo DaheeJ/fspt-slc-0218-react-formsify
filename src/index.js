c1b66fbe3080783cb79f994ca80f98c19851dabd
Wes Cutting <wesleycutting@gmail.com>
2018-03-14 18:14:54 -0600

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './components/Login'
+
+const App = () => (
+  <div>
+    <Login/>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
